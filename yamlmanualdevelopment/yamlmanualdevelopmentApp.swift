f4b0a0c5b6db14d9eddef13b93410fc197616988
VeArnold <arnold@nevercode.io>
2020-10-22 11:07:25 +0300

    Initial Commit

---
+++ yamlmanualdevelopment/yamlmanualdevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  yamlmanualdevelopmentApp.swift
+//  yamlmanualdevelopment
+//
+//  Created by Codemagic on 22.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct yamlmanualdevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
